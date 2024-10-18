ca7cb0258a4d3c6fc08573191f4dead72407e1af
AndreaZanasi <zanasi.ndr@gmail.com>
2024-10-18 08:48:57 +0200

    Refactor routes and templates for sign in functionality

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-
-@app.route('/register')
+@app.route('/signin')
 def register():
-    return render_template('register.html')
+    return render_template('signin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
