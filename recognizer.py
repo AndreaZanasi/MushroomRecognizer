6d2f9d70d894050f2526105d0978567e19985f47
Andrea Zanasi <129591951+AndreaZanasi@users.noreply.github.com>
2024-10-19 09:54:02 +0200

    Refactor image preprocessing and model loading

--- recognizer.py
+++ recognizer.py
@@ -7,9 +7,9 @@ import torch.nn as nn
 # Define the predict_image function
 def predict(image_path, class_names):
     preprocess = transforms.Compose([
-        transforms.Resize([250, 250]),
+        transforms.Resize([350, 350]),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.0208, 0.0254, 0.0203], std=[0.9948, 0.9935, 0.9932])
+        transforms.Normalize(mean=[0.2787, 0.2223, 0.1592], std=[0.2433, 0.2235, 0.2131])
     ])
 
     image = Image.open(image_path).convert('RGB')
@@ -28,6 +28,6 @@ def load_model():
     number_of_classes = 9
     model.fc = nn.Linear(num_ftrs, number_of_classes)
 
-    model.load_state_dict(torch.load(r'weights/model_weights(lr=0.01,mom=0.9,wd=0.003,pretr=Yes,bs=32,ep=10,size=250).pth', map_location=torch.device('cpu'), weights_only=True))
+    model.load_state_dict(torch.load(r'weights\model_weights(lr=0.01,mom=0.9,wd=0.003,pretr=Yes,bs=32,ep=50,size=350,trainacc=98.07864164432529,testacc=98.8978254393804).pth', map_location=torch.device('cpu'), weights_only=True))
     model.eval()
     return model
